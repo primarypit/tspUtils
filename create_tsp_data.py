9419142dcf438303d70407aeee8f2e2c62c139ba
primarypit <2895123212@qq.com>
2024-04-14 11:01:17 +0800

    update

--- create_tsp_data.py
+++ create_tsp_data.py
@@ -25,7 +25,7 @@ for filename in os.listdir(Tspfolder):
 sample_files = []
 sample_files += random.sample(sample_100,1)
 sample_files += random.sample(sample_1000,1)
-sample_files += random.sample(sample_ohter,1)
+#sample_files += random.sample(sample_ohter,1)
 left_files = list(set(os.listdir(Tspfolder)) - set(sample_files))
 
 minv = 0
@@ -36,12 +36,12 @@ for f in sample_files:
     tmp_matrix = tmp.return_matrix()
     tsp_test_data[f.split('.')[0]] = tmp_matrix / np.max(tmp_matrix) # normalize to [0,1]
 
-with open('tspTestset_F.py', 'w') as file:
+with open('tspTestset_NS.py', 'w') as file:
     # Write the dictionary as code to the file
     file.write("import numpy as np\n\n")
     file.write("sample_files = " + str(sample_files) + "\n\n")
     file.write("left_files = " + str(left_files) + "\n\n")
-    file.write('tsp_test_data = {\n')
+    file.write('tsp_test_data_NS = {\n')
     for key, value in tsp_test_data.items():
         file.write("    \"{}\": np.array({}),\n".format(key,value.tolist()))
     file.write('}')
